c8f8c7802a0f7ecfed355689e7422e3dec43395d
NazRinRus <NazRinRus@gmail.com>
2023-10-01 20:43:10 +0300

    Выполнено: 1) Класс хранения данных Fototable; 2) Класс обработки данных FototableDataProcessor    2.1) Метод создание json файла с данными;
    Задачи:
    1) Создать шаблон docx-файла для выгрузки в него данных фототаблицы;
    2) Создать класс для обработки данных объекта фототаблицы и добавления его в шаблон, с сохранением под именем КУСП

--- fototable_lib.py
+++ fototable_lib.py
@@ -5,12 +5,36 @@ from config import keys
 from telebot import types
 from datetime import datetime
 
-doc = docx.Document('files/fototable_sample.docx')
+kusp = '4560'
+date_kusp = '01.10.2023'
+kusp_info = f'{kusp} от {date_kusp}'
+images = [
+    {"name": "files/380009616/3124/file_11.jpg", "photo_description": "\u0421\u0447\u0435\u0442\u0447\u0438\u043a"},
+    {"name": "files/380009616/3124/file_12.jpg", "photo_description": "\u0421\u0447\u0451\u0442\u0447\u0438\u043a \u0441\u043d\u0442 \u0436\u0435\u043b\u0435\u0437\u043d\u043e\u0434\u043e\u0440\u043e\u0436\u043d\u0438\u043a"}
+]
 
-paragraph1 = doc.add_paragraph('Это первый абзац')
-paragraph2 = doc.add_paragraph('Это второй абзац')
+# блок работы с шаблоном фототаблицы
+doc = docx.Document('files/fototable_sample.docx')#открываю экземпляр шаблона фототаблицы
 
-paragraph1.add_run('этот текст добавлен в первый абзац')
-paragraph2.add_run('этот текст добавлен во сторой абзац')
+# получаю объект верхнего колонтитула
+header = doc.sections[0].header
+title = header.tables[0] # таблица с информацией о КУСП (номер КУСП и дата)
+kusp_cell = title.cell(0, 1) # ячейка содержащая информацию о КУСП
+kusp_cell.text = kusp_info # текст ячейки содержащей номер КУСП
+
+# Содержание фототаблицы
+number_of_rows = len(images)*2 #количество строк в таблице равно количеству фотографий умноженное на 2 (строка с пояснениями)
+table1 = doc.add_table(rows=number_of_rows, cols=1) # построение объекта таблицы
+# заполнение таблицы данными
+print(number_of_rows)
+for row in range(number_of_rows):
+    print(row)
+    cell = table1.cell(row, 0)
+    if (row % 2) == 0:
+        cell.text = images[row // 2]["name"]
+    if (row % 2) == 1:
+        cell.text = images[row // 2]["photo_description"]
+
+
 
 doc.save('files/380009616/3124/fototable.docx')
