726ed5fd006f8161890030f17909ecaac0f98701
NazRinRus <NazRinRus@gmail.com>
2023-10-01 22:30:06 +0300

    Выполнено: 1) Класс хранения данных Fototable; 2) Класс обработки данных FototableDataProcessor    2.1) Метод создание json файла с данными;
    Задачи:
    1) Создать шаблон docx-файла для выгрузки в него данных фототаблицы;
    2) Создать класс для обработки данных объекта фототаблицы и добавления его в шаблон, с сохранением под именем КУСП

--- fototable_lib.py
+++ fototable_lib.py
@@ -6,13 +6,15 @@ from config import keys
 from telebot import types
 from datetime import datetime
 
-kusp = '4560'
-date_kusp = '01.10.2023'
+# извлечение данных с файла json
+with open('files/380009616/3124/3124.json') as f:
+    file_content = f.read()
+    templates = json.loads(file_content)
+
+kusp = templates['kusp']
+date_kusp = templates['current_datetime']
 kusp_info = f'{kusp} от {date_kusp}'
-images = [
-    {"name": "files/380009616/3124/file_11.jpg", "photo_description": "\u0421\u0447\u0435\u0442\u0447\u0438\u043a"},
-    {"name": "files/380009616/3124/file_12.jpg", "photo_description": "\u0421\u0447\u0451\u0442\u0447\u0438\u043a \u0441\u043d\u0442 \u0436\u0435\u043b\u0435\u0437\u043d\u043e\u0434\u043e\u0440\u043e\u0436\u043d\u0438\u043a"}
-]
+images = templates['images']
 
 # блок работы с шаблоном фототаблицы
 doc = docx.Document('files/fototable_sample.docx')#открываю экземпляр шаблона фототаблицы
