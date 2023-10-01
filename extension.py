9e7197a4d493f1991a249278286eaad2ad908651
NazRinRus <NazRinRus@gmail.com>
2023-10-01 23:23:48 +0300

    Выполнено: 1) Класс хранения данных Fototable; 2) Класс обработки данных FototableDataProcessor    2.1) Метод создание json файла с данными; 3) Создать шаблон docx-файла для выгрузки в него данных фототаблицы; 4) Создать класс для обработки данных объекта фототаблицы и добавления его в шаблон, с сохранением под именем КУСП
    Задачи:
    1)Разработать метод, определяющий пользователя по его telegram_id, если его нет в списке пользователей, то послать его на заполнение данных, данные хранить в json-файле users;

--- extension.py
+++ extension.py
@@ -8,55 +8,6 @@ from datetime import datetime
 class APIException(Exception):
     pass
 
-# Основной класс
-class CryptoConverter:
-    # Метод отслеживающий нажатие кнопок в меню, принимает текс сообщения, возвращает True/Fаlse
-    @staticmethod
-    def checking_input(input_str: str):# Принимает текст сообщения от пользователя
-        pressing_button = False # Показатель срабатывания кнопки - пока не известно сообщение введено вручную или с кнопки
-        for i_key, i_value in keys.items(): # Проверяю по словарю валют
-            if(i_key + ' / ' + i_value) == input_str: # если строка "Ключ + / + значение" совпадает с текстом сообщения,
-                Menu_with_buttons.pressing_button += 1 # то была нажата соответсвующая Ключу кнопка и
-                pressing_button = True # показатель срабатывания кнопки в режим True
-                if Menu_with_buttons.pressing_button == 1: # Если кнопка из меню сработала первый раз, то
-                    Menu_with_buttons.values = [i_key] # ключ соответствует исходной валюте, ставим ее в начало списка и
-                    return pressing_button # возвращаем True - сообщение получено при нажатии кнопки
-                elif Menu_with_buttons.pressing_button == 2: # Если же кнопка сработала второй раз, то
-                    Menu_with_buttons.values.append(i_key)# ключ соответствует результирующей валюте, дополняем список и
-                    return pressing_button # возвращаем True - сообщение получено при нажатии кнопки
-        if Menu_with_buttons.pressing_button == 2: # Если сообщение отправлено пользователем не путем нажатия одной из кнопок,
-            try: # но при этом кнопки срабатывали дважды, возможно пользователь ввел количество исходной валюты
-                amount = float(input_str) # переводим сообщение пользователя в число, предворительно улавливая исключение
-                Menu_with_buttons.values.append(amount) # число добавляем в список как количество исходной валюты
-                Menu_with_buttons.pressing_button = 0 # обнуляем счетчик нажатия кнопок
-            except ValueError:
-                raise ConnectionError(f'Не верный формат количества валюты {input_str}, введите число')
-
-        return pressing_button # возвращаем False, так как последнее сообщение с количеством исходной валюты, введено в ручную
-
-    # Метод, проверяющий корректность запроса, отправляющий запрос на сервер, обрабатывающий поступивший контент
-    @staticmethod
-    def get_price(quote: str, base: str, amount: str): # принимает название исходной, результативной валют и количество исходной валюты
-        if quote == base: # проверка на совпадение исходной и результативной валют
-            raise APIException(f'Исходная и результативная валюты совпадают - {base}')
-        try: # присваиваю переменной quote_ticker значение ключа quote - исходная валюта
-            quote_ticker = keys[quote]  # ключ - устоявшееся название валюты, значение - официальное международное название
-        except KeyError: # если пользователь ввел название одной из валют с ошибкой, то сработает исключение
-            raise APIException(f'Не удалось обработать валюту {quote}')
-        try:
-            base_ticker = keys[base] # присваиваю переменной base_ticker значение ключа base - результирующая валюта
-        except KeyError:
-            raise APIException(f'Не удалось обработать валюту {base}')
-        try:
-            amount = float(amount)# проверка правильности ввода пользователем числа - количества исходной валюты
-        except ValueError:
-            raise ConnectionError(f'Не верный формат количества валюты {amount}, введите число')
-        # Отправляю сформированный запрос API на сервер и получаю ответ в переменную r
-        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
-        # из полученного ответа вычленяем стоимость валюты и присваиваем ее к переменной total_base
-        total_base = json.loads(r.content)[base_ticker]
-        return total_base * amount # возвращаем стоимость валюты, умноженную на количество
-
 # Класс формирующий меню кнопок
 class Menu_with_buttons:
     pressing_button = 0 # количество нажатий на кнопки, запрос формируется из двух последовательных нажатий и ручного ввода количества исходной валюты
